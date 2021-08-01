10fe7ab90b8c9a532e3d4f815a0741d427ba7f6e
Deserav <88135502+Deserav@users.noreply.github.com>
2021-08-01 15:46:34 +0900

    Add files via upload

--- allrDB_gene_set.py
+++ allrDB_gene_set.py
@@ -1,8 +1,11 @@
-f0 = open("D:/Desktop/scrapy/PubMed/allrDB_count.txt", "r")
+# f0 = open("D:/Desktop/scrapy/PubMed/allrDB_count.txt", "r")
+f0 = open("C:/Users/frigg/Desktop/Environmental-Disease-main/Environmental-Disease-main/allrDB_count.txt", "r")
+
 x = f0.readlines()
 
 disease = []
 genes = []
+final = []
 
 for i in range(len(x)):
     if i % 7 == 0:
@@ -17,9 +20,17 @@ for i in range(len(genes)):
         y = genes[i].index("{")
         genes[i] = genes[i][y + 1 : len(genes[i])]
     else:
-        genes.remove(genes[i])
+        genes[i] = ""
 
+for strings in genes:
+    final.extend(strings.replace("'", "").replace("}", "").split())
 
+# print(final)
+omega = list(set(final))
+print(len(omega)) # found a total of 5251 genes in "allrDB_count.txt"
 
+f1 = open("C:/Users/frigg/Desktop/allrDB_gene_set.txt", "w")
+y = f1.writelines(omega)
+# print(final)
 # print(disease)
 # print(genes)
